81a4f1b0e18269ccfbd08dd3f43ce06873890789
Corina <fluturelcorina@gmail.com>
2017-07-06 11:21:20 +0100

    Checking max temperature and power saving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,53 +1,77 @@
 
 describe("Thermostat", function(){
-
-   var thermostat;
-
+  
+  var thermostat;
+  
   beforeEach(function(){
-  thermostat = new Thermostat();
-   });
-
-   describe("#temperature", function(){
-     it("will be set to twenty at initialisation", function(){
-        expect(thermostat.temperature).toEqual(20);
-     });
-   });
-
-   describe("#up", function(){
-     it("will increase the temp by one degree", function(){
-       thermostat.up();
-       expect(thermostat.temperature).toEqual(21);
-     });
-   });
-
-   describe("#down", function(){
-     it("will decrease the temp by one degree", function(){
-       thermostat.down();
-       expect(thermostat.temperature).toEqual(19);
-     });
-   });
-
-   describe("#checkMinimum", function(){
-     it("will throw an error if the temp is below 10 degrees", function() {
-       for(var i = 20; i >= 10; i--) {
-         thermostat.down();
-       };
-       expect( function() { thermostat.checkMinimum() } ).toThrow(new Error("Brrr"));
-     });
-   });
-
-   describe("#powerSavingModeOn", function(){
-     it("has power saving mode on by default", function() {
-       expect(thermostat.powerSavingModeOn).toEqual(true);
-     });
-   });
-
-   describe("#maxTemp", function(){
-     it("has a maximum temperature of 25 degrees", function() {
-      for(var i = 20; i <= 25; i++) {
-        thermostat.up();
+    thermostat = new Thermostat();
+  });
+  
+  describe("#temperature", function(){
+    it("will be set to twenty at initialisation", function(){
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+  
+  describe("#up", function(){
+    it("will increase the temp by one degree", function(){
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(21);
+    });
+  });
+  
+  describe("#down", function(){
+    it("will decrease the temp by one degree", function(){
+      thermostat.down();
+      expect(thermostat.temperature).toEqual(19);
+    });
+  });
+  
+  describe("#checkMinimum", function(){
+    it("will throw an error if the temp is below 10 degrees", function() {
+      for(var i = 20; i > 10; i--) {
+        thermostat.down();
       };
+      expect( function() { thermostat.checkMinimum() } ).toThrow(new Error("Brrr"));
+    });
+  });
+  
+  describe("#powerSavingModeOn", function(){
+    it("has power saving mode on by default", function() {
+      expect(thermostat.powerSavingModeOn).toEqual(true);
+    });
+  });
+  
+  describe("#powerSavingModeSwitch", function() {
+    it("changes the value of powerSavingModeOn", function() {
+      thermostat.powerSavingModeSwitch();
+      expect(thermostat.powerSavingModeOn).toEqual(false);
+    });
+    
+    it("changes the value of powerSavingModeOn", function() {
+      thermostat.powerSavingModeSwitch();
+      thermostat.powerSavingModeSwitch();
+      expect(thermostat.powerSavingModeOn).toEqual(true);
+    });
+    
+  });
+  
+  describe("#maxTemp", function(){
+    it("has a maximum temperature of 25 degrees when power saving mode is on", function() {
+      for(var i = 20; i < 25; i++) {
+        thermostat.up();
+      }
       expect( function() { thermostat.checkMax() } ).toThrow(new Error("Cannot go beyond 25"));
+    });
+    
+    it("has a maximum temperature of 32 degrees when power saving mode is off", function() {
+      thermostat.powerSavingModeSwitch();
+      for (var i = 20; i < 32; i++) {
+        thermostat.up();
+      }
+      expect( function() {
+        thermostat.checkMax()
+      }).toThrow(new Error("Cannot go beyond 32"));
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,25 +3,36 @@ function Thermostat(){
   this.temperature = 20,
   this.minTemp = 10,
   this.powerSavingModeOn = true,
-  this.maxTempPSMOn = 25
+  this.maxTempPSMOn = 25,
+  this.maxTempPSMOff = 32
 }
 
 Thermostat.prototype.up = function() {
+  this.checkMax();
   this.temperature++;
 };
 
 Thermostat.prototype.down = function() {
+  this.checkMinimum();
   this.temperature--;
 };
 
 Thermostat.prototype.checkMinimum = function() {
   if (this.temperature <= this.minTemp) {
     throw new Error("Brrr");
-  };
+  }
 };
 
 Thermostat.prototype.checkMax = function() {
-  if (this.temperature >= this.maxTempPSMOn) {
+  if (this.temperature >= this.maxTempPSMOn && this.powerSavingModeOn === true) {
     throw new Error("Cannot go beyond 25");
-  };
+  }
+  if (this.temperature >= this.maxTempPSMOff && this.powerSavingModeOn === false) {
+    throw new Error("Cannot go beyond 32");
+  }
+};
+
+
+Thermostat.prototype.powerSavingModeSwitch = function () {
+  return this.powerSavingModeOn = !this.powerSavingModeOn;
 };

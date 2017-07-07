2748614644f4b95d3c3e1f89467ccc1473f64808
Miho Tse <mihotse@gmail.com>
2017-07-07 14:43:01 +0100

    Add color change for usage

--- src/thermostatjQuery.js
+++ src/thermostatjQuery.js
@@ -5,18 +5,36 @@ $(document).ready(function(){
   $('#min-temp').text(thermostat.minTemp);
   $('#readPSM').text(thermostat.readPSM);
   $('#usage').text(thermostat.usage);
+    $("#usage").css("color", "orange");
+
   $("#Up").on("click", function() {
     thermostat.up();
     thermostat.currentEnergyUsage();
+    if (thermostat.usage === "low-usage") {
+      $("#usage").css("color", "green");
+    } else if (thermostat.usage === "medium-usage"){
+      $("#usage").css("color", "orange");
+    } else {
+      $("#usage").css("color", "red");
+    }
     $('#current-temp').text(thermostat.temperature);
     $('#usage').text(thermostat.usage);
   });
+
   $("#Down").on("click", function() {
     thermostat.down();
     thermostat.currentEnergyUsage();
+    if (thermostat.usage === "low-usage") {
+      $("#usage").css("color", "green");
+    } else if (thermostat.usage === "medium-usage"){
+      $("#usage").css("color", "orange");
+    } else {
+      $("#usage").css("color", "red");
+    }
     $('#current-temp').text(thermostat.temperature);
     $('#usage').text(thermostat.usage);
   });
+  
   $("#Reset").on("click", function() {
     thermostat.reset();
     $('#current-temp').text(thermostat.temperature);
